cfb15102a3ed0c4ceb2ad7dd7a5a95cbee10b2f1
Reverend Homer <mk.43.ecko@gmail.com>
2016-12-29 13:45:07 +0300

    reworking

--- CursesMenu.py
+++ CursesMenu.py
@@ -1,56 +1,49 @@
 # -*- coding: utf-8 -*-
 
 import curses
+import sys
 
 
 class CursesMenu(object):
     """docstring for CursesMenu"""
-    def __init__(self, headerstr, items):
-        self._screen = curses.initscr()
+    def __init__(self, screen, headerstr, items=[]):
+        self._screen = screen
         self._headerstr = headerstr
         self._items = items
         self.num_sel = 0
         curses.start_color()
+        curses.noecho()
         curses.init_pair(1, curses.COLOR_RED, curses.COLOR_WHITE)
         self._screen.keypad(1)
 
-    def __del__(self):
-        try:
-            curses.endwin()
-        except TypeError:
-            pass
-
+    def add(self, text, cb):
+        self._items.append({'text': text, 'func': cb})
 
     def drawHeader(self, invoke=True):
         self._screen.erase()
         self._screen.border(0)
-        """Initial values of vertical and horizontal indent"""
+        # Initial values of vertical and horizontal indent
         y = 2
         x = 2
-        """
-        Lovely pep8 analyzer things that long lines is PURE EVIL!!!
-        So I've had to declarate local variable for strings
-        """
         s = self._headerstr
         self._screen.addstr(y, x, s, curses.A_STANDOUT)
         y += 2
         if invoke:
-            s = "Please choose one of the next items"
             self._screen.addstr(y, x, s, curses.A_BOLD)
-        """That was header. Main content will be put little to the right"""
-        y += 1
+        # That was header. Main content will be put little to the right
         x += 2
-        return y, x
+        y += 1
+        return x, y
 
     def draw(self):
-        """Styles for sel and not sel menu items"""
+        # Styles for sel and not sel menu items
         SEL = curses.color_pair(1)
         NOT_SEL = curses.A_NORMAL
-        y, x = self.drawHeader()
+        x, y = self.drawHeader()
         num = 0
         for i in self._items:
             style = SEL if num == self.num_sel else NOT_SEL
-            self._screen.addstr(y, x, '{0}. {1}'.format(num, i), style)
+            self._screen.addstr(y, x, '{0}. {1}'.format(num, i['text']), style)
             num += 1
             y += 1
         y += 1
@@ -60,40 +53,42 @@ class CursesMenu(object):
         ch = self._screen.getch()
         return self.processInput(ch)
 
+    def up(self):
+        if self.num_sel == 0:
+            return 0
+        self.num_sel -= 1
+        return 0
+
+    def down(self):
+        if self.num_sel == len(self._items):
+            return 0
+        self.num_sel += 1
+        return 0
+
+    def run(self):
+        c = 0
+        while c >= 0:
+            c = self.draw()
+
+    def print(self, s):
+        self.drawHeader(False)
+        self._screen.addstr(15, 15, s, curses.A_NORMAL)
+        return 0
+
+    def process_handlers(self):
+        if self.num_sel == len(self._items):
+            return -1
+        return self._items[self.num_sel]['func']()
+
     def processInput(self, c):
-        def increaseNum():
-            if self.num_sel == len(self._items):
-                return -1
-            self.num_sel += 1
-            return -1
-
-        def decreaseNum():
-            if self.num_sel == 0:
-                return -1
-            self.num_sel -= 1
-            return -1
-
-        def showItem():
-            if self.num_sel == len(self._items):
-                correctExit()
-            y, x = self.drawHeader(False)
-            s = '{}: {}'.format(self.num_sel, self._items[self.num_sel])
-            self._screen.addstr(14, 15, "You chose: ", curses.A_NORMAL)
-            self._screen.addstr(15, 15, s, curses.A_BOLD)
-            self._screen.getch()
-            return self.num_sel
-
-        def correctExit():
-            curses.endwin()
-            exit()
-
+        stop = lambda : -1
         handlers = {
-            ord('0'): correctExit,
-            ord('q'): correctExit,
-            ord('Q'): correctExit,
-            ord('\n'): showItem,  # dunno why curses.KEY_ENTER doesn't work :(
-            curses.KEY_UP: decreaseNum,
-            curses.KEY_DOWN: increaseNum
+            ord('0'): stop,
+            ord('q'): stop,
+            ord('Q'): stop,
+            ord('\n'): self.process_handlers,
+            curses.KEY_UP: self.up,
+            curses.KEY_DOWN: self.down
         }
         if c not in handlers:
             self._screen.erase()
@@ -101,6 +96,6 @@ class CursesMenu(object):
             s = "input {0}('{1}') was not recognized".format(c, cc)
             self._screen.addstr(15, 15, s, curses.A_BOLD)
             self._screen.getch()
-            return -1
+            return 0
         else:
             return handlers[c]()
--- l.py
+++ l.py
@@ -1,8 +1,22 @@
 #!/usr/bin/env python3
 
+import curses
 from CursesMenu import CursesMenu
 
+def foo():
+    print('baaaang!')
+    return 0
+
 if __name__ == '__main__':
-    m = CursesMenu('menu', ['one', 'two', 'three'])
-    while True:
-        m.draw()
+    s = curses.initscr()
+    m = CursesMenu(s, 'menu')
+    m.add('one', foo)
+    m.add('two', foo)
+    m.add('three', foo)
+    try:
+        m.run()
+    except Exception:
+        curses.endwin()
+        raise
+    else:
+        curses.endwin()
